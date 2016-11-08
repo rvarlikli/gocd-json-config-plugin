df8833de95ddbd25a3c5092a22ee9cd3cfae3869
Ramazan VARLIKLI <rvarlikli@yahoo.com>
2016-11-08 11:43:01 +0300

    Update ConfigDirectoryParser.java
    Modified for exported pipeline json file.
    Plugin pipeline pattern must set **/*.*.gopipeline.json This pattern means **/pipeline_group.pipeline_name.gopipeline.json

--- src/com.tw.go.config.json/ConfigDirectoryParser.java
+++ src/com.tw.go.config.json/ConfigDirectoryParser.java
@@ -1,13 +1,12 @@
 package com.tw.go.config.json;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
+import com.google.gson.*;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class ConfigDirectoryParser {
     private ConfigDirectoryScanner scanner;
@@ -55,6 +54,7 @@ public class ConfigDirectoryParser {
         }
 
         for (String pipelineFile : scanner.getFilesMatchingPattern(baseDir, pipelinePattern)) {
+            String pipelineGroup = pipelineFile.substring(0,pipelineFile.indexOf("."));
             try {
                 JsonElement pipe = parser.parseFile(new File(baseDir, pipelineFile));
                 if(pipe == null || pipe.isJsonNull())
@@ -71,8 +71,10 @@ public class ConfigDirectoryParser {
                             pipelineFile);
                     config.addError(error);
                 }
-                else
-                    config.addPipeline(pipe,pipelineFile);
+                else {
+                    pipe = fixJson(pipe,pipelineGroup);
+                    config.addPipeline(pipe, pipelineFile);
+                }
             }
             catch (JsonParseException parseException)
             {
@@ -84,5 +86,61 @@ public class ConfigDirectoryParser {
         }
 
         return config;
+    }
+
+    public JsonElement fixJson(JsonElement jsonElement, String pipelineGroup)
+    {
+        jsonElement.getAsJsonObject().addProperty("group",pipelineGroup);
+
+        ///removing attributes in materials section
+        JsonElement materialsElement = jsonElement.getAsJsonObject().get("materials");
+        JsonArray materialsArray = materialsElement.getAsJsonArray();
+        for(int i=0 ; i< materialsArray.size();i++){
+            JsonElement materialObject =  materialsArray.get(i);
+            JsonObject attributesObject = materialObject.getAsJsonObject().get("attributes").getAsJsonObject();
+            Set<Map.Entry<String, JsonElement>> entries = attributesObject.entrySet();//will return members of your object
+            for (Map.Entry<String, JsonElement> entry: entries) {
+                materialObject.getAsJsonObject().add(entry.getKey(), entry.getValue());
+            }
+            materialObject.getAsJsonObject().remove("attributes");
+        }
+        /// stages approval section
+        JsonElement stagesElement = jsonElement.getAsJsonObject().get("stages");
+        JsonArray stagesArray = stagesElement.getAsJsonArray();
+        for(int i= 0; i<stagesArray.size(); i++){
+            JsonElement stagesObject = stagesArray.get(i);
+            JsonElement approvalObject = stagesObject.getAsJsonObject().get("approval");
+            JsonObject authorizationObject = approvalObject.getAsJsonObject().get("authorization").getAsJsonObject();
+            Set<Map.Entry<String, JsonElement>> entries = authorizationObject.entrySet();
+            for (Map.Entry<String, JsonElement> entry: entries) {
+                approvalObject.getAsJsonObject().add(entry.getKey(), entry.getValue());
+            }
+            approvalObject.getAsJsonObject().remove("authorization");
+
+            JsonElement jobsElement = stagesObject.getAsJsonObject().get("jobs");
+            JsonArray jobsArray = jobsElement.getAsJsonArray();
+            for(int j= 0; j < jobsArray.size(); j++){
+                JsonElement jobsObject = jobsArray.get(j);
+                JsonElement jobsTasksElement = jobsObject.getAsJsonObject().get("tasks");
+                JsonArray jobsTasksArray = jobsTasksElement.getAsJsonArray();
+
+                for (int k= 0; k< jobsTasksArray.size(); k++ )
+                {
+                    JsonElement tasksObject = jobsTasksArray.get(k);
+                    JsonObject tasksAttributesObject = tasksObject.getAsJsonObject().get("attributes").getAsJsonObject();
+                    Set<Map.Entry<String, JsonElement>> entriesAttributes = tasksAttributesObject.entrySet();
+                    for (Map.Entry<String, JsonElement> entry: entriesAttributes) {
+                        if(entry.getKey().equals("run_if"))
+                        {
+                            JsonArray tempValue = entry.getValue().getAsJsonArray();
+                            entry.setValue(tempValue.get(0));
+                        }
+                        tasksObject.getAsJsonObject().add(entry.getKey(), entry.getValue());
+                    }
+                    tasksObject.getAsJsonObject().remove("attributes");
+                }
+            }
+        }
+        return jsonElement;
     }
 }
